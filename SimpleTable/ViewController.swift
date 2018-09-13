5225b6a4663847f8f843563317f3e0b4ee504ed8
Robert Dunn <robertdunn220@gmail.com>
2018-09-13 19:03:31 -0400

    Test

--- SimpleTable/ViewController.swift
+++ SimpleTable/ViewController.swift
@@ -8,7 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakah", "Cafe Loisl", "Petite Oyster",
+                           "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery",
+                           "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif",
+                           "Graham Avenue Meats and Deli", "Waffle & Wolf", "Five Leaves",
+                           "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak",
+                           "CASK Pub and Kitchen"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows in the section
+        
+        return restaurantNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier:
+            cellIdentifier, for: indexPath)
+        
+        // Configure the cell...
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
